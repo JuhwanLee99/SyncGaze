0b5dc011478446852951269545f643ca6804ff95
JuhwanLee99 <jimmy3470second@gmail.com>
2025-11-16 02:40:35 +0900

    fix: landing page가 안나오는 문제 해결

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -3,16 +3,23 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.tsx'
+import './App.css'
 import './index.css'
 
 // 1. WebgazerProvider를 import 합니다.
 import { WebgazerProvider } from './context/WebgazerContext.tsx'
 
+// 2. react-router-dom에서 BrowserRouter를 import 합니다.
+import { BrowserRouter } from 'react-router-dom';
+
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    {/* 2. <App /> 컴포넌트를 <WebgazerProvider>로 감싸줍니다. */}
-    <WebgazerProvider>
-      <App />
-    </WebgazerProvider>
+    {/* 3. BrowserRouter를 최상위로 이동시켜 앱 전체를 감쌉니다. */}
+    <BrowserRouter>
+      {/* 4. <App /> 컴포넌트를 <WebgazerProvider>로 감싸줍니다. */}
+      <WebgazerProvider>
+        <App />
+      </WebgazerProvider>
+    </BrowserRouter>
   </React.StrictMode>,
 )
